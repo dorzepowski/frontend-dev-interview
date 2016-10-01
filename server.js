3c953a1bb3a0d7a3fcd86f9c4f68e3c669fe4eac
Damian Orzepowski <info@softwarelab.com.pl>
2016-10-01 21:05:35 +0200

    Add get menu endpoint

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var app = express();
 
-
 app.use(express.static('web'));
+
+app.get('/api/menu', function (req, res) {
+    var menu = [];
+    menu.push(createMenuItem("home"));
+    menu.push(createMenuItem("about"));
+    menu.push(createMenuItem("projects"));
+    menu.push(createMenuItem("contact"));
+    res.send(menu);
+
+    function createMenuItem(name) {
+        var item = {};
+        item.name = name;
+        item.label = capitalize(name);
+        item.href = "#" + capitalize(name);
+        return item;
+    }
+});
+
 
 app.listen(8123, function () {
     console.log('Starts server on port ', 8123);
 });
+
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
